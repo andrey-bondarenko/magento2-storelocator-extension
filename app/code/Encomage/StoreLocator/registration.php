1f8d498e86be6c33e7949f5e1e2dd843a0e1ad8d
Andrey Bondarenko <andrey.bondarenko@ewave.com>
2017-02-02 19:23:00 +0200

    Added sceleton

---
+++ app/code/Encomage/StoreLocator/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package com.encomage.storelocator.m2
+ * @author Andrey Bondarenko
+ * @link http://encomage.com
+ * @mail info@encomage.com
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Encomage_StoreLocator',
+    __DIR__
+);
